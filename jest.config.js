63eebd7f9f10bff852d262dad459a01d73d3949e
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2024-07-26 16:25:35 -0400

    moved jest to swc so next can build successfully

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,24 @@
-// jest.config.js
 module.exports = {
   testEnvironment: 'node',
   setupFiles: ['<rootDir>/jest.setup.js'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+    '^.+\\.(js|jsx|ts|tsx)$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: {
+            syntax: 'ecmascript', // or 'typescript' for .ts/.tsx files
+            importMeta: true
+          },
+          transform: {
+            decoratorMetadata: false
+          }
+        },
+        module: {
+          type: 'es6' // Use 'es6' for ES Modules
+        }
+      }
+    ]
   },
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/']
 };
